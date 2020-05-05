37548fe24adf47a2f26c5f07e79c879856233283
Niki Manoledaki <18622989+nikimanoledaki@users.noreply.github.com>
2020-05-05 08:53:59 +0100

    Returns formatted cron job with one line

--- lib/editor.rb
+++ lib/editor.rb
@@ -1,14 +1,20 @@
 class Editor
-    attr_reader :config, :time
+    attr_reader :fields, :current_time
 
-    def get(config, time)
-        @config = config
-        @time = time
+    def get(config, current_time)
+        @fields = config.split(' ')
+        @current_time = Time.new(current_time)
     end
 
     def put
-        puts @config
-        puts @time
+        cron_time = "#{@fields[1]}:#{@fields[0]}"
+        command = fields[2]
+        puts "#{cron_time} #{day?(cron_time)} - #{command}"
+    end
+
+    def day?(cron_time)
+        today = @current_time <= Time.new(cron_time)
+        day = today == true ? "today" : "tomorrow"
     end
 
 end
--- spec/editor_spec.rb
+++ spec/editor_spec.rb
@@ -2,15 +2,23 @@ require 'editor.rb'
 
 describe Editor do
     describe '#put' do
-        it 'returns formatted list' do
+        it 'returns formatted cron job with one line' do
             time = "16:01"
             config = "30 1 /bin/run_me_daily\n"
-            subject.get(time, config)
+            subject.get(config, time)
             result = "1:30 tomorrow - /bin/run_me_daily\n"
             expect{subject.put}.to output(result).to_stdout
         end
 
         # it 'returns formatted list' do
+        #     time = "16:01"
+        #     config = "* * /bin/run_me_daily\n"
+        #     subject.get(config, time)
+        #     result = "16:01 today - /bin/run_me_daily\n"
+        #     expect{subject.put}.to output(result).to_stdout
+        # end
+
+        # it 'returns formatted list of cron jobs' do
         #     time = "16:01"
         #     config = ["30 1 /bin/run_me_daily\n", "45 * /bin/run_me_hourly\n", "* * /bin/run_me_every_minute \n", "* 19 /bin/run_me_sixty_times\n"]
         #     result = "1:30 tomorrow - /bin/run_me_daily\n16:45 today - /bin/run_me_hourly\n16:10 today - /bin/run_me_every_minute\n19:00 today - /bin/run_me_sixty_times"
